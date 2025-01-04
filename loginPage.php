d2ae8663171d1affd4a3ff698639b0fe6d691b41
faisalrizqin <faisalrizqitwenty7@gmail.com>
2025-01-04 22:34:15 +0700

    ubah judul form login

--- loginPage.php
+++ loginPage.php
@@ -65,7 +65,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <!-- nav -->
     <nav class="navbar navbar-expand-lg bg-body-tertiary sticky-top">
         <div class="container">
-          <a class="navbar-brand" href="#">My Daily Journal</a>
+          <a class="navbar-brand" href="#">Welcome to My Daily Journal</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
